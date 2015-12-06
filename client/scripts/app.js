a2b182971b114395df6303fc85faf28f667946cf
Danny Ritter <Danny@Taras-MacBook-Air.local>
2015-12-05 19:39:44 -0600

    Make small change to radio buttons

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute', 'ngMaterial', 'ngMessages']);
+var myApp = angular.module('myApp', ['ngRoute','ngAria', 'ngMaterial', 'ngMessages']);
 
 //Routing!
 myApp.config(['$routeProvider', function($routeProvider){
